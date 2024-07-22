368b38abdcc73f363e1b67f72f4234d7afcd79b4
Evyonne <evyonnemuriuki@gmail.com>
2024-07-23 00:58:03 +0300

    Corrections on assignment. The alert message now displays appropriately.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-document.getElementById('userForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector("button").onclick=ageBracket
+});
+       
+function ageBracket(){
     const age = parseInt(document.getElementById('age').value);
     let responseMessage;
-
-    if (age < 15) {
-        responseMessage = 'Hello' ${username}, 'you are a Gen Alpha.';
+    if (age >0 && age < 15) {
+        responseMessage=("Gen Alpha");
     } else if (age >= 15 && age < 25) {
-        responseMessage = "Hello" ${username}, "you are Gen-Z.";
+        responseMessage=("Gen-Z");
     } else if (age >= 25 && age < 35) {
-        responseMessage = "Hello" ${username}, "you are a millenial.";
+        responseMessage=("Millenial");
     } else if (age >= 35 && age < 45) {
-        responseMessage = Hello ${username}, you are a bloomer.;
+        responseMessage=("Gen-X");
     } else if (age >= 45 && age < 65 ) {
-        responseMessage = Hello ${username}, you are Gen-X.;
-    } 
-    } else {
-        responseMessage = Hello ${username}, you are a senior citizen.;
+        responseMessage=("Bloomer");
+    } else if (age >= 65 && age < 120){
+        responseMessage=("Senior citizen");
+    }else{
+        responseMessage = ("Invalid entry")
     }
-
-    document.getElementById('response').textContent = responseMessage;
-    
-});
+    alert(responseMessage)
+}
